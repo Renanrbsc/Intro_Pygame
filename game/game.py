a8b3e65fba9ab45357190443dedb298177bb2de3
unknown <900159@proway.treina>
2019-12-06 11:55:38 -0300

    .

--- game/game.py
+++ game/game.py
@@ -2,19 +2,31 @@ import pygame
 
 largura_tela = 850
 altura_tela = 500
-
+speed = 10
 pygame.init()
 tela = pygame.display.set_mode((largura_tela,altura_tela))
 
 
-plano_fundo = pygame.image.load('Intro_pygame\game\RvaMB7.png')
-plano_fundo = pygame.transform.scale(plano_fundo,(largura_tela,altura_tela))
 
+
+class move(pygame.sprite.Sprite):
+
+    def __init__(self,largura_tela,altura_tela):
+
+        self.plano_fundo = pygame.image.load('Intro_pygame\game\RvaMB7.png')
+        self.plano_fundo = pygame.transform.scale(plano_fundo,(largura_tela,altura_tela))    
+        self.rect = self.image.get_rect()
+
+    def update():
+        self.rect[0] -= speed
 
 while True:
      
-        
-    tela.blit(plano_fundo,(0,0))
+    move_move = pygame.sprite.Group()
+    move = move(largura_tela, 100)
+    move_move.append(move)
+    
+    # tela.blit(plano_fundo,(0,0))
         
     pygame.display.update()
 
