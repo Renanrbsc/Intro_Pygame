39ad56fbea8f62fd4d97023338f09ee520189e60
Renan Berti PC <renanrbsc@gmail.com>
2019-12-07 12:58:15 -0300

    corrigindo pulo

--- game/Renan_python.py
+++ game/Renan_python.py
@@ -4,8 +4,9 @@ from pygame.locals import *
 # CONSTANTES
 largura_tela = 900 # WIDTH X
 altura_tela = 450  # HEIGHT Y
-PULO = 50
-gravidade = 2
+PULO = 20
+SPEED= 15
+gravidade = 10
 
 class Ninja(pygame.sprite.Sprite): # modela um objeto do mundo real
                                    # atribui variaveis para atributos e funcoes para comportamento
@@ -20,7 +21,12 @@ class Ninja(pygame.sprite.Sprite): # modela um objeto do mundo real
                        pygame.image.load('Intro_pygame\game\\ninja run\\2d-game-sprite-6_004a.png').convert_alpha(),
                        ]
 
+        # Variaveis da Classe Ninja
         self.pulo = PULO
+        self.largura_tela = largura_tela
+        self.altura_tela = altura_tela
+        self.velocidade = SPEED
+        self.gravidade = gravidade
 
         # contador vinculado as imagens
         self.sequencia_image = 0
@@ -29,27 +35,34 @@ class Ninja(pygame.sprite.Sprite): # modela um objeto do mundo real
         self.image = pygame.image.load('Intro_pygame\game\\ninja run\\2d-game-sprite-6_001a.png').convert_alpha() 
         self.rect = self.image.get_rect() 
         self.rect[0] = 100 # primeira posicao tupla posicionamento x
-        self.rect[1] = altura_tela / 2 +90 # segunda posicao tupla posicionamento y
+        self.rect[1] = 315 # segunda posicao tupla posicionamento y
                
     def pular(self):     # move personagem no eixo y
-        self.rect[1] -= self.pulo
+        for i in range(self.pulo): # loop na quantidade de pixels
+            self.rect[1] -= gravidade/5 # valor de pulo em relacao a metade da gravidade em 'loop'
+            if  self.rect[1] < -50: #limitador de movimento Y
+                self.rect[1] += self.pulo
 
     def direita(self):   # move personagem no eixo x
-        self.rect[0] += 20
+        self.rect[0] += self.velocidade
+        if self.rect[0] > 830: #limitador de movimento X
+            self.rect[0] -= self.velocidade
     
     def esquerda(self):  # move personagem no eixo x
-        self.rect[0] -= 20
+        self.rect[0] -= self.velocidade
+        if self.rect[0] < 20: #limitador de movimento X
+            self.rect[0] += self.velocidade
 
+    def gravidade_ninja(self):    
+        self.rect[1] += self.gravidade # atualizacao de gravidade eixo y do personagem
+        if self.rect[1] == 325: # limitador para personagem permanecer na tela
+            self.rect[1] -= self.gravidade # anula gravidade no eixo y '325'
+        
     def update(self):
         self.sequencia_image = (self.sequencia_image + 1) % 4   #  % 4 zera o contador 
         self.image = self.images[self.sequencia_image]          #  e reseta a sequancia de image
-        self.rect[1] += gravidade # atualizacao de gravidade eixo y do personagem
-        if self.rect[1] == 325: # limitador para personagem permanecer na tela
-            self.rect[1] -= gravidade # anula gravidade no eixo y '325'
-
-
-
-
+        self.gravidade_ninja() # chamando metodo de gravidade para atualizaçao
+        
 pygame.init()
 tela = pygame.display.set_mode((largura_tela,altura_tela)) #definindo janela e tamanho
 
@@ -66,27 +79,23 @@ while True: # loop principal do game
 
     fps.tick(20) # chama metodo limitar FPS 
      
-    for event in pygame.event.get(): # evento de sair da janela
-        if event.type == QUIT:
+    for event in pygame.event.get(): # verifica evento de janela
+        if event.type == QUIT: #verifica evento sair
             pygame.quit()
-        
-        #verifica teclado
-        if event.type == KEYDOWN:
-            #verifica pulo
-            if event.key == K_SPACE:
+       
+    if event.type == KEYDOWN: #verifica teclado
+        if event.key == K_UP: #verifica pulo
                 ninja.pular()
-            
-            #verifica direita 
-            if event.key == K_LEFT:
-                ninja.esquerda()
+           
+        if event.key == K_LEFT: #verifica esquerda
+            ninja.esquerda()
                 # if event.key == K_LEFT:   # funcao para inverter imagem ao mudar lado
                 #     ninja.image = pygame.transform.flip(ninja.image,True,False)
                 # else:
                 #     ninja.image = pygame.transform.flip(ninja.image,False,False)
-               
-            #verifica direita
-            elif event.key == K_RIGHT:
-                ninja.direita()
+       
+        if event.key == K_RIGHT: #verifica direita
+            ninja.direita()
 
     tela.blit(plano_fundo,(0,0)) # coloca plano de fundo na posicao x y
 
