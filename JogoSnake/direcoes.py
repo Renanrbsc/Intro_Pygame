3cb0b19e425bde840fc857d9b02bc0d3cc2d3d72
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-07 18:42:28 -0300

    mudando diretorio da turtle

--- JogoSnake/direcoes.py
+++
@@ -1,2 +0,0 @@
-def DIREITA(K_RIGHT):
-    
--- Turtle_teste/RandonWalkerBasic.py
+++
@@ -1,9 +0,0 @@
-import turtle
-from random import randint
-
-t1 = turtle.Turtle()
-t1.speed(0)
-
-while True:
-  t1.forward(5)
-  t1.right(randint(-45,45))
